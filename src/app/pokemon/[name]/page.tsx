d7e0551e8160f3ac9825fde85c7da5d75e516bf0
Hanief Burhanuddin <haniefburhan17@gmail.com>
2024-11-05 20:54:48 +0700

    fix: mobile view for pokemen detail

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -27,11 +27,14 @@ export default function Pokemon() {
   const About = pokemonDetailData?.about;
 
   return (
-    <div className='flex w-fit flex-col gap-10 lg:p-4'>
+    <div className='flex w-full max-w-md flex-col gap-6 p-4 sm:max-w-lg lg:max-w-xl'>
       {navContent.map((item) => (
-        <div key={item.key} className='flex justify-between gap-20'>
-          <div className='font-bold'>{item.label}:</div>
-          <div>
+        <div
+          key={item.key}
+          className='flex flex-col items-start justify-between gap-1 sm:flex-row sm:gap-4'
+        >
+          <div className='text-sm font-bold sm:text-base'>{item.label}:</div>
+          <div className='text-sm sm:text-base'>
             {item.key === 'abilities'
               ? About?.abilities.join(', ')
               : item.key === 'species'
